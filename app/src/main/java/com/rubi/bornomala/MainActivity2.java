029e0dc41fb8a22ed76449afe057c8d3ec671143
MD NASRULLAH <mn.limon.239@gmail.com>
2023-05-07 20:43:16 +0600

    audio sound added

--- app/src/main/java/com/rubi/bornomala/MainActivity2.java
+++ app/src/main/java/com/rubi/bornomala/MainActivity2.java
@@ -2,13 +2,25 @@ package com.rubi.bornomala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+    }
+
+    public void callClick(View view) {
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.click);
+        mediaPlayer.start();
     }
 }

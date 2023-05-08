62d9fd5f59d8a6f8a0e90667213e285ea07ad954
MD NASRULLAH <mn.limon.239@gmail.com>
2023-05-08 12:01:29 +0600

    coding

--- app/src/main/java/com/rubi/bornomala/MainActivity2.java
+++ app/src/main/java/com/rubi/bornomala/MainActivity2.java
@@ -1,10 +1,12 @@
 package com.rubi.bornomala;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -14,6 +16,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         setTitle("আরবি হরফ সমূহ");
 
@@ -35,5 +40,12 @@ public class MainActivity2 extends AppCompatActivity {
     public void Click(View view) {
         final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.click);
         mediaPlayer.start();
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

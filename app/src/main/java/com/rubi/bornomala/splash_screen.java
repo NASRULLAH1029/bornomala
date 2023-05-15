84648c341bd251ba098a77de7737b651eb3eac67
MD NASRULLAH <mn.limon.239@gmail.com>
2023-05-15 10:35:21 +0600

    new code added

--- app/src/main/java/com/rubi/bornomala/splash_screen.java
+++ app/src/main/java/com/rubi/bornomala/splash_screen.java
@@ -1,6 +1,10 @@
 package com.rubi.bornomala;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -10,6 +14,27 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                try {
+                    sleep(4000);
+
+                    Intent intent =new Intent(splash_screen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+            }
+        });
+        thread.start();
+
     }
 }

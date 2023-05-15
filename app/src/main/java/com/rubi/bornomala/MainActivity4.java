baf870be01267d1328c27e8f60a9dd256fb55afa
MD NASRULLAH <mn.limon.239@gmail.com>
2023-05-15 11:35:35 +0600

    cd

--- app/src/main/java/com/rubi/bornomala/MainActivity4.java
+++ app/src/main/java/com/rubi/bornomala/MainActivity4.java
@@ -3,6 +3,7 @@ package com.rubi.bornomala;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -10,5 +11,114 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+    }
+
+    public void C1(View view) {
+    }
+
+    public void C2(View view) {
+    }
+
+    public void C3(View view) {
+    }
+
+    public void C4(View view) {
+    }
+
+    public void C5(View view) {
+    }
+
+    public void C6(View view) {
+    }
+
+    public void C7(View view) {
+    }
+
+    public void C8(View view) {
+    }
+
+    public void C9(View view) {
+    }
+
+    public void C10(View view) {
+    }
+
+    public void C11(View view) {
+    }
+
+    public void C12(View view) {
+    }
+
+    public void C13(View view) {
+    }
+
+    public void C14(View view) {
+    }
+
+    public void C15(View view) {
+    }
+
+    public void C16(View view) {
+    }
+
+    public void C17(View view) {
+    }
+
+    public void C18(View view) {
+    }
+
+    public void C19(View view) {
+    }
+
+    public void C20(View view) {
+    }
+
+    public void C21(View view) {
+    }
+
+    public void C22(View view) {
+    }
+
+    public void C23(View view) {
+    }
+
+    public void C24(View view) {
+    }
+
+    public void C25(View view) {
+    }
+
+    public void C26(View view) {
+    }
+
+    public void C27(View view) {
+    }
+
+    public void C28(View view) {
+    }
+
+    public void C29(View view) {
+    }
+
+    public void C30(View view) {
+    }
+
+    public void C31(View view) {
+    }
+
+    public void C32(View view) {
+    }
+    public void C33(View view) {
+    }
+    public void C34(View view) {
+    }
+    public void C35(View view) {
+    }
+    public void C36(View view) {
+    }
+    public void C37(View view) {
+    }
+
+    public void C38(View view) {
     }
 }

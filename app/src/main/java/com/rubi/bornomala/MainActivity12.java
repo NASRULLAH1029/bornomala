2065eaf9bca5f372fe05ef294c4c874a931ea37d
MD NASRULLAH <mn.limon.239@gmail.com>
2023-06-18 21:44:48 +0600

    audio

--- app/src/main/java/com/rubi/bornomala/MainActivity12.java
+++ app/src/main/java/com/rubi/bornomala/MainActivity12.java
@@ -3,8 +3,10 @@ package com.rubi.bornomala;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity12 extends AppCompatActivity {
 
@@ -17,6 +19,41 @@ public class MainActivity12 extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+    public void b1(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.e_day1);
+        mediaPlayer.start();
+    }
+
+    public void b2(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.e_day2);
+        mediaPlayer.start();
+    }
+
+    public void b3(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.e_day3);
+        mediaPlayer.start();
+    }
+
+    public void b4(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.e_day4);
+        mediaPlayer.start();
+    }
+
+    public void b5(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.e_day5);
+        mediaPlayer.start();
+    }
+
+    public void b6(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.e_day6);
+        mediaPlayer.start();
+    }
+
+    public void b7(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.e_day7);
+        mediaPlayer.start();
+    }
+
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             this.finish();
@@ -25,4 +62,6 @@ public class MainActivity12 extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
 
     }
+
+
 }

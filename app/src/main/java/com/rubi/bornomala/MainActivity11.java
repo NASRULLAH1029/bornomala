1ec82aaf523d7cbdca29ac0cc5c36fb023ad1baf
MD NASRULLAH <mn.limon.239@gmail.com>
2023-06-18 15:08:08 +0600

    nhtgh

--- app/src/main/java/com/rubi/bornomala/MainActivity11.java
+++ app/src/main/java/com/rubi/bornomala/MainActivity11.java
@@ -25,62 +25,62 @@ public class MainActivity11 extends AppCompatActivity {
 
     public void b1(View view) {
 
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_1);
         mediaPlayer.start();
     }
 
     public void b2(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_2);
         mediaPlayer.start();
     }
 
     public void b3(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_3);
         mediaPlayer.start();
     }
 
     public void b4(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_4);
         mediaPlayer.start();
     }
 
     public void b5(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_5);
         mediaPlayer.start();
     }
 
     public void b6(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_6);
         mediaPlayer.start();
     }
 
     public void b7(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_7);
         mediaPlayer.start();
     }
 
     public void b8(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_8);
         mediaPlayer.start();
     }
 
     public void b9(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_9);
         mediaPlayer.start();
     }
 
     public void b10(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_10);
         mediaPlayer.start();
     }
 
     public void b11(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_11);
         mediaPlayer.start();
     }
 
     public void b12(View view) {
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bn_m_12);
         mediaPlayer.start();
     }
 

71d6d314c904c329a651ea16aa7d5a0dc84fb37f
MD NASRULLAH <mn.limon.239@gmail.com>
2023-06-18 15:04:11 +0600

    jholu

--- app/src/main/java/com/rubi/bornomala/MainActivity11.java
+++ app/src/main/java/com/rubi/bornomala/MainActivity11.java
@@ -3,8 +3,10 @@ package com.rubi.bornomala;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity11 extends AppCompatActivity {
 
@@ -15,6 +17,71 @@ public class MainActivity11 extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        setTitle("বাংলা ১২ মাসের নাম");
+
+
+    }
+
+    public void b1(View view) {
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b2(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b3(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b4(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b5(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b6(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b7(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b8(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b9(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b10(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b11(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
+    }
+
+    public void b12(View view) {
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.b_ritu_6);
+        mediaPlayer.start();
     }
 
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -25,4 +92,6 @@ public class MainActivity11 extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
 
     }
+
+
 }

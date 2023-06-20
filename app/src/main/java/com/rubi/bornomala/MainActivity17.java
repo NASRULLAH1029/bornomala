eb32d8a6d21ccfe17d57e8dc9789f1463a32350d
MD NASRULLAH <mn.limon.239@gmail.com>
2023-06-20 11:48:19 +0600

    full

--- app/src/main/java/com/rubi/bornomala/MainActivity17.java
+++ app/src/main/java/com/rubi/bornomala/MainActivity17.java
@@ -10,5 +10,8 @@ public class MainActivity17 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main17);
+
+
+        setTitle("ফুৃলের নাম");
     }
 }

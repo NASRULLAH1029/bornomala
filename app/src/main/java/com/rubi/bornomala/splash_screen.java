0338aed3df8ee465148c43b034a9f670f4816426
MD NASRULLAH <mn.limon.239@gmail.com>
2023-09-09 09:08:34 +0600

    fgh

--- app/src/main/java/com/rubi/bornomala/splash_screen.java
+++ app/src/main/java/com/rubi/bornomala/splash_screen.java
@@ -2,11 +2,13 @@ package com.rubi.bornomala;
 
 import static java.lang.Thread.sleep;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -21,15 +23,14 @@ public class splash_screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        // For Hide Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         text1 = findViewById(R.id.textView);
-
-
         Splash_bottom = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
-
         text1.setAnimation(Splash_bottom);
-
-
 
         //For Splash Screen
         final Handler handler = new Handler();
@@ -41,7 +42,7 @@ public class splash_screen extends AppCompatActivity {
                 startActivity(myIntent);
                 finish();
             }
-        }, 4000);
+        }, 3000);
         //==================================
     }
 }

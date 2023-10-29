49f5f978a05068d7da9fbfaf02a655a367c6f44c
MD NASRULLAH <mn.limon.239@gmail.com>
2023-10-29 13:34:07 +0600

    problem

--- app/src/main/java/com/rubi/bornomala/feedback.java
+++ app/src/main/java/com/rubi/bornomala/feedback.java
@@ -1,33 +1,90 @@
 package com.rubi.bornomala;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class feedback extends AppCompatActivity {
+
+    ImageView back;
+    CardView Call_me;
+    public String num = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_feedback);
+        // For Hide Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
-        setTitle("ডেভেলপার সস্পর্কে");
+        Call_me = findViewById(R.id.mycall);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        back = findViewById(R.id.back_developer);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                feedback.super.onBackPressed();
+
+            }
+        });
+
+        Call_me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num = "+8801723261029";
+                callPhoneNumber();
+            }
+        });
 
 
     }
 
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            this.finish();
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 101) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callPhoneNumber();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
+    public void callPhoneNumber() {
+        try {
+            if (Build.VERSION.SDK_INT > 22) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(feedback.this, new String[]{Manifest.permission.CALL_PHONE}, 101);
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + num));
+                startActivity(callIntent);
+
+            } else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + num));
+                startActivity(callIntent);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
